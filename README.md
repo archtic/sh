23f10fba438b5905585f4da75eef1e4e13a5f717
archtic <archtic_github@proton.me>
2022-06-12 15:18:02 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Archtic: Shell scripts
+
+## setup.sh
+
+`curl -o- {URL HERE} | bash`
+
+Installs packages and config files. Assumes a fresh Arch install.
---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Variables
+NVM_URL = "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh"
+OMZ_URL="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+V="v0.1" # Version number
+
+# Intro text
+echo -e "\n\n\u001b[36mA R C H\u001b[0m T I C \u001b[36m$V\u001b[0m\n\n"
+sleep 3
+
+# Install necessary packages
+yes | sudo pacman -Syu \
+    base-devel   \ # Build tools
+    iwd          \ # Wireless daemon
+    zsh          \ # Shell
+    xterm        \ # Fallback terminal emulator
+    alacritty    \ # Preferred erminal emulator
+    herbstluftwm \ # Window manager
+    neovim       \ # Editor
+    fontconfig   \ # Custom font support
+    ttf-dejavu   \ # Fallback font
+    chromium     \ # Web browser
+    git          \ # Source control
+    lazygit      \ # Git terminal UI
+    feh          \ # Image viewer
+    scrot        \ # Screen capture
+    htop         \ # Process viewer
+    wget         \ # Fetcher
+/
+
+# Install Paru
+git clone https://aur.archlinux.org/paru.git
+cd paru
+yes | makepkg -si
+cd ..
+rm -rf ./paru
+
+# Paru packages
+paru -Syu \
+    nerd-fonts-cascadia-code \ # Preferred font
+    dotbare                  \ # Dotfile manager
+
+/
+
+wget -qO- $NVM_URL | bash             # Install NVM
+yes | sh -c "$(wget $OMZ_URL -O-)"    # Install OMZ
+
+# Finalize
+fc-cache                               # Refresh fonts
