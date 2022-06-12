8416e560f5766ee526317e5c27ef2ece401017d7
archtic <archtic_github@proton.me>
2022-06-12 15:38:22 -0700

    Add aliases

--- setup.sh
+++ setup.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
-# Variables
-NVM_URL = "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh"
+# Aliases / variables
+alias pac='yes "" | sudo pacman -S'
+alias aur='yes "" | paru -S'
+NVM_URL="https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh"
 OMZ_URL="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 V="v0.1" # Version number
+
 
 # Intro text
 echo -e "\n\n\u001b[36mA R C H\u001b[0m T I C \u001b[36m$V\u001b[0m\n\n"
 
 # Install necessary packages
-yes "" | sudo pacman -Syu \
-    base-devel   \ # Build tools
-    iwd          \ # Wireless daemon
-    zsh          \ # Shell
-    xterm        \ # Fallback terminal emulator
-    alacritty    \ # Preferred erminal emulator
-    herbstluftwm \ # Window manager
-    neovim       \ # Editor
-    fontconfig   \ # Custom font support
-    ttf-dejavu   \ # Fallback font
-    chromium     \ # Web browser
-    git          \ # Source control
-    lazygit      \ # Git terminal UI
-    feh          \ # Image viewer
-    scrot        \ # Screen capture
-    htop         \ # Process viewer
-    wget         \ # Fetcher
-/
+sudo pacman -Syu
+pac base-devel   # Build tools
+pac iwd          # Wireless daemon
+pac zsh          # Shell
+pac xterm        # Fallback terminal emulator
+pac alacritty    # Preferred erminal emulator
+pac herbstluftwm # Window manager
+pac neovim       # Editor
+pac fontconfig   # Custom font support
+pac ttf-dejavu   # Fallback font
+pac chromium     # Web browser
+pac git          # Source control
+pac lazygit      # Git terminal UI
+pac feh          # Image viewer
+pac scrot        # Screen capture
+pac htop         # Process viewer
+pac wget         # Fetcher
 
 # Install Paru
 git clone https://aur.archlinux.org/paru.git
@@ -36,11 +38,9 @@ cd ..
 rm -rf ./paru
 
 # Paru packages
-paru -Syu \
-    nerd-fonts-cascadia-code \ # Preferred font
-    dotbare                  \ # Dotfile manager
-
-/
+paru -Syu
+aur nerd-fonts-cascadia-code # Preferred font
+aur dotbare                  # Dotfile manager
 
 wget -qO- $NVM_URL | bash             # Install NVM
 yes "" | sh -c "$(wget $OMZ_URL -O-)"    # Install OMZ
