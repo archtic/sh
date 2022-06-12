f6e85fc3a48b1f95e609fa2ffb3e54b677c5b669
archtic <archtic_github@proton.me>
2022-06-12 15:48:20 -0700

    Whitespace

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ NVM_URL="https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh"
 OMZ_URL="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 V="v0.1" # Version number
 
-
 # Intro text
 echo -e "\n\n\u001b[36mA R C H\u001b[0m T I C \u001b[36m$V\u001b[0m\n\n"
 
@@ -43,7 +42,7 @@ aur nerd-fonts-cascadia-code # Preferred font
 aur dotbare                  # Dotfile manager
 
 wget -qO- $NVM_URL | bash             # Install NVM
-yes "" | sh -c "$(wget $OMZ_URL -O-)"    # Install OMZ
+yes "" | sh -c "$(wget $OMZ_URL -O-)" # Install OMZ
 
 # Finalize
-fc-cache                               # Refresh fonts
+fc-cache # Refresh fonts
